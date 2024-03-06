774c71c2c10505ddd29ab1a3beb097addff2f63c
VZyma <112830236+VZyma@users.noreply.github.com>
2024-03-06 06:12:20 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,12 @@
 function filterArray(numbers, value) {
-    let filterArray = [];
-    for (i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            filterArray.push(numbers[i]);
+    let filteredArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if(numbers[i] > value) {
+            filteredArray.push(numbers[i]);
         }
     }
-    return filterArray;
+    return filteredArray;
 }
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
